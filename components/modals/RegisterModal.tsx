a3694e6882bd589de9888bdfb6e1a1acd665b8ce
Rika Miyata <rika13tay@gmail.com>
2023-10-20 11:16:43 +0900

    Connect register account to database (MongoDB)

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -3,6 +3,9 @@ import { useCallback, useState } from "react";
 import Input from "../Input";
 import Modal from "../Modal";
 import useRegisterModal from "@/hooks/useRegisterModal";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 const RegisterModal = () => {
   const loginModal = useLoginModal();
@@ -26,15 +29,29 @@ const RegisterModal = () => {
   const onSubmit = useCallback(async () => {
     try {
       setIsLoading(true);
-      // TODO ADD REGISTER AND LOGIN
+      
+      await axios.post('/api/register', {
+        email,
+        password,
+        username,
+        name
+      });
+
+      toast.success('Account created.');
+
+      signIn('credentials', {
+        email,
+        password
+      });
 
       registerModal.onClose();
     } catch (error) {
       console.log(error);
+      toast.error('Something went wrong');
     } finally {
       setIsLoading(false);
     }
-  }, [loginModal]);
+  }, [registerModal, email, password, username, name]);
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
