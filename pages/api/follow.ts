c67880135b83e08f80a13fc54bb94a2dfdc452fd
Rika Miyata <rika13tay@gmail.com>
2023-10-24 12:25:19 +0900

    Create notifications

--- pages/api/follow.ts
+++ pages/api/follow.ts
@@ -19,7 +19,7 @@ export default async function handler(
       throw new Error('Invalid ID');
     }
 
-    const user = await prisma.findUnique({
+    const user = await prisma.user.findUnique({
       where: {
         id: userId
       }
@@ -33,6 +33,26 @@ export default async function handler(
 
     if (req.method === 'POST') {
       updatedFollowingIds.push(userId);
+
+      try {
+        await prisma.notification.create({
+          data: {
+            body: 'Someone followed you!',
+            userId
+          }
+        });
+        
+        await prisma.user.update({
+          where: {
+            id: userId
+          },
+          data: {
+            hasNotification: true
+          }
+        })
+      } catch (error) {
+        console.log(error);
+      }
     }
 
     if (req.method === 'DELETE') {
